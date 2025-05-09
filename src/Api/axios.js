3b804ee86d88be8c0dd784180d6fa3923d5027ed
fuzail vhora <fuzailvhora144@gmail.com>
2025-05-09 15:20:11 +0530

    Remove Authorization header from axios instance configuration

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -8,7 +8,6 @@ const axiosInstance = axios.create({
     headers:{
         "Content-Type": "application/json",
         "Accept": "application/json",
-        "Authorization" : `${token}`
     }
   });
   
