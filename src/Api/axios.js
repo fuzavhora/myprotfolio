fdafee4f3edf1ce957cc2ab7d680e5afa7c32843
fuzail vhora <fuzailvhora144@gmail.com>
2025-05-09 16:29:48 +0530

    Set default base URL for axios instance if REACT_APP_API_URL is not defined

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "https://portfolio-server-sr3n.onrender.com/project",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
